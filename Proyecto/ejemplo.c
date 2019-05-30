0349bec18327d46cedf59884bc869779bb3bdf71
Piste <piste.gomez96@gmail.com>
2019-05-29 22:11:17 -0500

    Proyecto

---
+++ Proyecto/ejemplo.c
@@ -0,0 +1,57 @@
+int main(){
+	int x, y;
+
+	x= 0;
+   
+	struct compiladores_ejemplo{
+		int a;
+		float b;
+		char c;
+	};
+
+	switch(v){
+	case 5: x = 0; v++; break;
+	case 'a': x = 0; v++; break;
+	}
+
+	if (x>0){
+		for (y=1; y<6;y++){
+			x++;
+		}
+	}
+	else
+	{
+		y=6;
+		while(y>0){
+			x++;
+			y--;
+		}
+	}
+
+	for (y=1; y<6;y++){
+		for (y=1; y<6;y++){
+			x++;
+		}
+	}	
+
+	return (0);
+}
+
+int funcion(int i, float f){
+	i=0;
+	f=i+1;
+
+	struct estructura2{
+		int x;
+		float _y_;
+		char z;
+	};
+
+   return (9);
+}
+
+void nulo(){
+	do{
+		x = x-1;
+	}while(x > 0);
+}
